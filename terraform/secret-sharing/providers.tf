0e05c7fa03edb1c14e47f53099440825139f2177
Rishabh Mulani <rishabh.mulani@ba.com>
2025-09-09 18:35:45 +0100

    Removed dupes

--- terraform/secret-sharing/providers.tf
+++ terraform/secret-sharing/providers.tf
@@ -1,9 +1,14 @@
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "6.9.0"
     }
+    docker = {
+        source  = "kreuzwerker/docker"
+        version = "3.6.2"
+      }
   }
 }
 
