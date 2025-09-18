1c9b4fe9d175ad1d9b3b40aa5494f32b7e1f1c56
Rishabh Mulani <rishabh.mulani@ba.com>
2025-09-18 10:39:39 +0100

    Redeploy all resources to test destroy

--- terraform/secret-sharing-infra/ecr.tf
+++ terraform/secret-sharing-infra/ecr.tf
@@ -2,8 +2,9 @@ resource "aws_ecr_repository" "secret_sharing" {
   name = "ecr-mercury-${var.env}-euwe1-shared-service_secret-sharing"
 
   image_tag_mutability = "MUTABLE"
-#   image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
+  
   image_scanning_configuration {
-     scan_on_push = true
-   }
+    scan_on_push = true
+  }
 }
